a73b255d25307115c8186e0219f028fe572469e5
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-06-19 21:32:55 -0600

    Updating JS classes and factories practice file

--- jsTasks/w04d01/homework.js
+++ jsTasks/w04d01/homework.js
@@ -1,26 +1,21 @@
 console.log('----------------------------------------Hamster---------------------------------------------------')
 class Hamster{
-    constructor(owner, name, price){
-      this.owner = owner;
+    constructor(name){
+      this.owner = '';
       this.name = name;
-      this.price = price;
+      this.price = 15;
     }
     wheelRun(){
-      console.log(`When hamster named ${this.name} running the wheel, you can hear 'squeak squeak'`);
+      console.log("squek, squek");
     }
     eatFood(){
-      console.log(`When hamster named ${this.name} is eating, you can hear 'nibble nibble'`);
+      console.log("nibble, nibble");
     }
     getPrice(){
-      return ` ${this.owner} bought  ${this.name}  for $${this.price}`;
+      return this.price;
     }
   }
-  const action1 = new Hamster('Billy', 'Pumpkin', 100);
-  action1.wheelRun();
-  action1.eatFood();
-  console.log(action1.getPrice())
-  
-  
+ 
   console.log('----------------------------------------Person---------------------------------------------------')
  
   class Person  {
@@ -41,7 +36,7 @@ class Hamster{
     }
   
     getAge() {
-        return `John is ${this.age} old`;
+        return this.age;
     }
   
     getWeight() {
@@ -61,8 +56,8 @@ class Hamster{
       this.weight--; 
     }
   
-    ageUp(num1) {
-      this.age+=num1+1;
+    ageUp() {
+      this.age++;
       this.height++;
       this.weight++;
       this.mood--;
@@ -72,25 +67,79 @@ class Hamster{
     buyHamster(hamster) {
       this.hamsters.push(hamster);
       this.mood += 10;
-      this.bankAccount -= hamster.price;
+      this.bankAccount -= hamster.getPrice();
     }
   }
   
-  const person = new Person("John");
-  console.log(`${person.getName()} is here!`); 
-  person.greet(); 
-  person.eat();
-  person.exercise();
-  person.ageUp(50);
-  console.log(person.getAge()); 
-  console.log(`${person.name} is getting older. Age: ${person.age}, Height: ${person.height}, Weight: ${person.weight}, the mood scale from 0 to 10 is  ${person.mood}, Bank Account: ${person.bankAccount}`)
+  console.log('-------------------Story------------------');
+
+  const person1 = new Person('Timmy');
+
+  for(let i = 0; i<5; i++){
+   person1.ageUp();
+   }
+   console.log(person1)
+console.log(`${person1.name}'s age is ${person1.getAge()}`)
+
+  for( let i = 0; i<5; i++){
+    person1.eat();
+  }
+  console.log(`After eating 5 times a day ${person1.name} gained ${person1.weight} lbs`)
+  
+  for(let i = 0; i<5; i++){
+    person1.exercise();
+  }
+  console.log(`After exercising consistently ${person1.name} lost  ${person1.weight} lbs`)
 
   
-  const hamster = new Hamster(person.name, 'Puff', 105)
-  person.buyHamster(hamster);
-  console.log(`${person.hamsters[0].owner} has a pet hamster named '${person.hamsters[0].name}', he bought '${person.hamsters[0].name}' for $${person.hamsters[0].price} `); 
-  console.log(`${person.hamsters[0].owner}'s mood on the scale 0 to 10, is ${person.mood}`); 
-  console.log(`Even though his bank acount balance is $${person.bankAccount}`); 
-  
-  console.log('-------------------Story------------------');
-  
+for( let i = 5; i<9; i++){
+    person1.ageUp();
+    }
+    console.log(`${person1.name}'s age is ${person1.getAge()}`)
+
+const hamster1 = new Hamster('Gus');
+
+hamster1.owner = person1.name;
+
+console.log(`Hamster ${hamster1.name}'s owner is ${hamster1.owner}`);
+   
+person1.buyHamster(hamster1);
+
+console.log(person1)
+
+for (i= 9; i<15; i++){
+  person1.ageUp()
+}
+for (i=0; i<2; i++){
+  person1.eat();
+  person1.exercise()
+}
+console.log(person1)
+
+console.log('-------------------------------Chef Make Dinners----------------------------');
+
+class Dinner {
+  constructor(appetizer, entree, dessert){
+    this.appetizer = appetizer;
+    this.entree = entree;
+    this.dessert = dessert;
+  }
+}
+
+class Chef{
+  cook(first, second, third){
+    return new Dinner(first, second, third)
+  }
+
+}
+
+const chef = new Chef();
+const dinner1 = chef.cook('Salad', 'Steak', 'Chocolate Cake');
+console.log(dinner1);
+const dinner2 = chef.cook('Soup', 'Chicken', 'Ice-Cream');
+console.log(dinner2);
+const dinner3 = chef.cook('Wings', 'Seafood Extravaganza', 'Apple Pie');
+console.log(dinner3);
+console.log(`Your ${dinner1.appetizer} is served!`)
+console.log(`Your ${dinner2.entree} is served!`)
+console.log(`Your ${dinner3.dessert} is served!`)
